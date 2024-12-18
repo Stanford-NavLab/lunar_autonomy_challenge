978254ab9c491042caacafb972e600a0b679d117
Adam Dai <adamdai97@gmail.com>
2024-12-17 16:53:04 -0800

    Logging stereo, poses/imu and semantic images

--- agents/data_collection_agent.py
+++ agents/data_collection_agent.py
@@ -13,6 +13,9 @@ import carla
 import cv2 as cv
 import random
 import os
+import json
+import jsonlines
+import time
 from math import radians
 from pynput import keyboard
 import plotly.graph_objects as go
@@ -39,13 +42,24 @@ class DataCollectionAgent(AutonomousAgent):
 
         """ Initialize a counter to keep track of the number of simulation steps. """
         self.frame = 0
-        self.rate = 5  # Sub-sample rate. Max rate is 10Hz
-
-        self.poses = []
+        self.rate = 1  # Sub-sample rate. Max rate is 10Hz
 
         self.run_name = "data_collection"
+
+        self.log_file = "output/" + self.run_name + "/data_log.json"
+        self.out = {
+            "initial_pose": transform_to_numpy(self.get_initial_position()).tolist(),
+            "lander_pose": transform_to_numpy(self.get_initial_lander_position()).tolist(),
+        }
+        # TODO: add camera and lights config
+
+        self.frames = []
+
         if not os.path.exists("output/" + self.run_name):
-            os.makedirs("output/" + self.run_name + "/images")
+            os.makedirs("output/" + self.run_name + "/front_left")
+            os.makedirs("output/" + self.run_name + "/front_right")
+            os.makedirs("output/" + self.run_name + "/front_left_semantic")
+            os.makedirs("output/" + self.run_name + "/front_right_semantic")
 
     def use_fiducials(self):
         """We want to use the fiducials, so we return True."""
@@ -64,16 +78,17 @@ class DataCollectionAgent(AutonomousAgent):
             },
             carla.SensorPosition.FrontLeft: {
                 "camera_active": True,
-                "light_intensity": 0,
+                "light_intensity": 1.0,
                 "width": "1280",
                 "height": "720",
                 "use_semantic": True,
             },
             carla.SensorPosition.FrontRight: {
-                "camera_active": False,
-                "light_intensity": 0,
+                "camera_active": True,
+                "light_intensity": 1.0,
                 "width": "1280",
                 "height": "720",
+                "use_semantic": True,
             },
             carla.SensorPosition.Left: {
                 "camera_active": False,
@@ -114,8 +129,10 @@ class DataCollectionAgent(AutonomousAgent):
             self.set_front_arm_angle(radians(60))
             self.set_back_arm_angle(radians(60))
 
-        grayscale_data = input_data["Grayscale"][carla.SensorPosition.FrontLeft]
-        semantic_data = input_data["Semantic"][carla.SensorPosition.FrontLeft]
+        FL_img = input_data["Grayscale"][carla.SensorPosition.FrontLeft]
+        FR_img = input_data["Grayscale"][carla.SensorPosition.FrontRight]
+        FL_sem = input_data["Semantic"][carla.SensorPosition.FrontLeft]
+        FR_sem = input_data["Semantic"][carla.SensorPosition.FrontRight]
 
         current_pose = transform_to_numpy(self.get_transform())
         camera_to_robot_pose = transform_to_numpy(
@@ -126,25 +143,48 @@ class DataCollectionAgent(AutonomousAgent):
 
         """ We need to check that the sensor data is not None before we do anything with it. The data for each camera will be 
         None for every other simulation step, since the cameras operate at 10Hz while the simulator operates at 20Hz. """
-        if grayscale_data is not None:
-            cv.imshow("Left camera view", grayscale_data)
+        if FL_img is not None:
+            cv.imshow("Left camera view", FL_img)
             cv.waitKey(1)
 
             if self.frame % self.rate == 0:
                 cv.imwrite(
-                    "output/" + self.run_name + "/images/" + str(self.frame) + ".png",
-                    grayscale_data,
+                    "output/" + self.run_name + "/front_left/" + str(self.frame) + ".png",
+                    FL_img,
+                )
+                cv.imwrite(
+                    "output/" + self.run_name + "/front_right/" + str(self.frame) + ".png",
+                    FR_img,
+                )
+                cv.imwrite(
+                    "output/" + self.run_name + "/front_left_semantic/" + str(self.frame) + ".png",
+                    FL_sem,
+                )
+                cv.imwrite(
+                    "output/" + self.run_name + "/front_right_semantic/" + str(self.frame) + ".png",
+                    FR_sem,
                 )
                 # self.poses.append(to_blender_convention(camera_to_world_pose))
-                self.poses.append(camera_to_world_pose)
+                log_entry = {
+                    "frame": self.frame,
+                    "timestamp": time.time(),
+                    "mission_time": self.get_mission_time(),
+                    "current_power": self.get_current_power(),
+                    "pose": camera_to_world_pose.tolist(),
+                    "imu": imu_data.tolist(),
+                }
+                self.frames.append(log_entry)
 
             self.frame += 1
 
         control = carla.VehicleVelocityControl(self.current_v, self.current_w)
 
         if self.frame % 100 == 0:
-            fig = go.Figure(data=pose_traces(self.poses))
-            fig.write_html("output/" + self.run_name + "/poses.html")
+            # fig = go.Figure(data=pose_traces(self.poses))
+            # fig.write_html("output/" + self.run_name + "/poses.html")
+            with open(self.log_file, "w") as f:
+                self.out["frames"] = self.frames
+                json.dump(self.out, f, indent=4)
 
         if self.frame >= 5000:
             self.mission_complete()
@@ -153,9 +193,14 @@ class DataCollectionAgent(AutonomousAgent):
 
     def finalize(self):
         print("Running finalize")
+        # self.writer.close()
+
+        with open(self.log_file, "w") as f:
+            json.dump(self.log_entries, f, indent=4)
+
         # Plot poses
-        fig = go.Figure(data=pose_traces(self.poses))
-        fig.write_html("output/" + self.run_name + "/poses.html")
+        # fig = go.Figure(data=pose_traces(self.poses))
+        # fig.write_html("output/" + self.run_name + "/poses.html")
 
         """In the finalize method, we should clear up anything we've previously initialized that might be taking up memory or resources.
         In this case, we should close the OpenCV window."""
